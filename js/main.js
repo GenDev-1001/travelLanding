6c7e12c2c12169e86aa7b1ff3026e7054ed586a0
GenDev-1001 <evgenii.ubileii@bk.ru>
2023-10-17 13:31:03 +0300

    feat: added project

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const burger = document.querySelector(".burger");
+const menuList = document.querySelector(".menu__list");
+
+const clickBurgerHandle = () => {
+	menuList.classList.toggle("menu__list-active");
+	burger.classList.toggle("burger-active");
+};
+
+burger.addEventListener("click", clickBurgerHandle);
+
+const swiper = new Swiper(".reviews__cards", {
+	slidesPerView: 1,
+	spaceBetween: 10,
+	breakpoints: {
+		600: {
+			slidesPerView: 2,
+			spaceBetween: 10,
+		},
+		1210: {
+			slidesPerView: 3,
+			spaceBetween: 25,
+		},
+	},
+});
